62e8893a1c08b8e081185207dd2812f2360f027c
LimBee <forestrainee@gmail.com>
2017-05-05 16:10:16 +0900

    Updated crop.sh

--- demo/compare/crop.sh
+++ demo/compare/crop.sh
@@ -1,24 +1,207 @@
-# Set 5
-th crop.lua -set Set5 -name baby -id 1 -lt 292_158 -ps 100
-th crop.lua -set Set5 -name baby -id 2 -lt 131_155 -ps 47
-th crop.lua -set Set5 -name baby -id 3 -lt 188_18 -ps 66
+Set 5
+# th crop.lua -set Set5 -name baby -id 1 -lt 292_158 -ps 100
+# th crop.lua -set Set5 -name baby -id 2 -lt 131_155 -ps 47
+# th crop.lua -set Set5 -name baby -id 3 -lt 188_18 -ps 66
 
-th crop.lua -set Set5 -name bird -id 1 -lt 137_29 -ps 67
-th crop.lua -set Set5 -name bird -id 2 -lt 218_31 -ps 57
+# th crop.lua -set Set5 -name bird -id 1 -lt 137_29 -ps 67
+# th crop.lua -set Set5 -name bird -id 2 -lt 218_31 -ps 57
 
-th crop.lua -set Set5 -name butterfly -id 1 -lt 139_121 -ps 70
-th crop.lua -set Set5 -name butterfly -id 2 -lt 195_78 -ps 48
+# th crop.lua -set Set5 -name butterfly -id 1 -lt 139_121 -ps 70
+# th crop.lua -set Set5 -name butterfly -id 2 -lt 195_78 -ps 48
 
-th crop.lua -set Set5 -name head -id 1 -lt 32_189 -ps 69
-th crop.lua -set Set5 -name head -id 2 -lt 131_45 -ps 43
+# th crop.lua -set Set5 -name head -id 1 -lt 32_189 -ps 69
+# th crop.lua -set Set5 -name head -id 2 -lt 131_45 -ps 43
 
-th crop.lua -set Set5 -name woman -id 1 -lt 12_32 -ps 38
-th crop.lua -set Set5 -name woman -id 2 -lt 40_167 -ps 49
-th crop.lua -set Set5 -name woman -id 3 -lt 47_74 -ps 37
+# th crop.lua -set Set5 -name woman -id 1 -lt 12_32 -ps 38
+# th crop.lua -set Set5 -name woman -id 2 -lt 40_167 -ps 49
+# th crop.lua -set Set5 -name woman -id 3 -lt 47_74 -ps 37
 
-# Set 14
+Set 14
 
-th crop.lua -set Set14 -name baboon -id 1 -lt 277_7 -ps 67
-th crop.lua -set Set14 -name baboon -id 2 -lt 175_272 -ps 123
-th crop.lua -set Set14 -name baboon -id 3 -lt 334_341 -ps 104
-th crop.lua -set Set14 -name baboon -id 4 -lt 13_48 -ps 96
+# th crop.lua -set Set14 -name baboon -id 1 -lt 277_7 -ps 67
+# th crop.lua -set Set14 -name baboon -id 2 -lt 175_272 -ps 123
+# th crop.lua -set Set14 -name baboon -id 3 -lt 334_341 -ps 104
+# th crop.lua -set Set14 -name baboon -id 4 -lt 13_48 -ps 96
+
+# th crop.lua -set Set14 -name barbara -id 1 -lt 44_352 -ps 55
+# th crop.lua -set Set14 -name barbara -id 2 -lt 179_21 -ps 31
+# th crop.lua -set Set14 -name barbara -id 3 -lt 292_299 -ps 23
+
+# th crop.lua -set Set14 -name comic -id 1 -lt 113_119 -ps 25
+# th crop.lua -set Set14 -name comic -id 2 -lt 59_158 -ps 20
+# th crop.lua -set Set14 -name comic -id 3 -lt 113_189 -ps 35
+
+# th crop.lua -set Set14 -name flowers -id 2 -lt 103_118 -ps 56
+# th crop.lua -set Set14 -name flowers -id 3 -lt 290_196 -ps 66
+# th crop.lua -set Set14 -name flowers -id 4 -lt 394_63 -ps 49
+
+# th crop.lua -set Set14 -name ppt3 -id 1 -lt 89_145 -ps 29
+# th crop.lua -set Set14 -name ppt3 -id 2 -lt 30_610 -ps 28
+# th crop.lua -set Set14 -name ppt3 -id 3 -lt 343_608 -ps 35
+# th crop.lua -set Set14 -name ppt3 -id 4 -lt 355_445 -ps 25
+# th crop.lua -set Set14 -name ppt3 -id 5 -lt 180_413 -ps 49
+# th crop.lua -set Set14 -name ppt3 -id 6 -lt 72_328 -ps 39
+# th crop.lua -set Set14 -name ppt3 -id 7 -lt 422_171 -ps 66
+# th crop.lua -set Set14 -name ppt3 -id 8 -lt 338_437 -wh 28_56
+
+# th crop.lua -set Set14 -name ppt3 -id 1 -lt 91_73 -wh 50_100
+# th crop.lua -set Set14 -name ppt3 -id 2 -lt 25_246 -wh 50_100
+# th crop.lua -set Set14 -name ppt3 -id 3 -lt 175_371 -wh 50_100
+# th crop.lua -set Set14 -name ppt3 -id 4 -lt 431_161 -wh 50_100
+# th crop.lua -set Set14 -name ppt3 -id 5 -lt 431_161 -wh 50_100
+
+# th crop.lua -set Set14 -name zebra -id 1 -lt 303_91 -wh 36_72
+# th crop.lua -set Set14 -name zebra -id 2 -lt 141_114 -wh 42_84
+# th crop.lua -set Set14 -name zebra -id 3 -lt 458_143 -wh 30_60
+
+# th crop.lua -set Set14 -name monarch -id 1 -lt 482_274 -wh 51_102
+# th crop.lua -set Set14 -name monarch -id 2 -lt 414_260 -wh 34_68
+# th crop.lua -set Set14 -name monarch -id 3 -lt 217_114 -wh 61_119
+# th crop.lua -set Set14 -name monarch -id 4 -lt 164_95 -wh 47_94
+
+# th crop.lua -set Set14 -name foreman -id 1 -lt 16_36 -wh 38_76
+# th crop.lua -set Set14 -name foreman -id 2 -lt 293_25 -wh 38_76
+
+B100
+
+# th crop.lua -set B100 -name 37073 -id 1 -lt 353_198 -wh 42_84
+# th crop.lua -set B100 -name 42012 -id 1 -lt 65_277 -wh 29_58
+# th crop.lua -set B100 -name 42012 -id 2 -lt 166_177 -wh 31_62
+# th crop.lua -set B100 -name 42049 -id 1 -lt 42_178 -wh 53_106
+# th crop.lua -set B100 -name 58060 -id 1 -lt 247_40 -wh 16_32
+# th crop.lua -set B100 -name 62096 -id 1 -lt 189_104 -wh 17_34
+# th crop.lua -set B100 -name 62096 -id 2 -lt 182_107 -wh 53_106
+# th crop.lua -set B100 -name 102061 -id 1 -lt 244_50 -wh 23_46
+# th crop.lua -set B100 -name 157055 -id 2 -lt 426_3 -wh 40_80
+# th crop.lua -set B100 -name 163085 -id 1 -lt 293_21 -wh 61_122
+# th crop.lua -set B100 -name 175032 -id 1 -lt 133_92 -wh 47_94
+# th crop.lua -set B100 -name 175032 -id 2 -lt 69_188 -wh 47_94
+# th crop.lua -set B100 -name 189080 -id 1 -lt 22_206 -wh 60_120
+# th crop.lua -set B100 -name 210088 -id 1 -lt 158_181 -wh 45_90
+# th crop.lua -set B100 -name 216081 -id 1 -lt 82_91 -wh 18_36
+
+# th crop.lua -set B100 -name 78004 -id 1 -lt 223_96 -wh 38_19
+# th crop.lua -set B100 -name 78004 -id 2 -lt 93_382 -wh 130_65
+# th crop.lua -set B100 -name 78004 -id 3 -lt 12_435 -wh 54_27
+# th crop.lua -set B100 -name 86000 -id 1 -lt 123_150 -wh 34_17
+# th crop.lua -set B100 -name 103070 -id 1 -lt 60_100 -wh 100_50
+# th crop.lua -set B100 -name 103070 -id 2 -lt 114_107 -wh 54_27
+# th crop.lua -set B100 -name 106024 -id 1 -lt 231_18 -wh 33_66
+# th crop.lua -set B100 -name 157055 -id 1 -lt 166_197 -wh 60_30
+# th crop.lua -set B100 -name 175032 -id 3 -lt 123_358 -wh 135_67
+# th crop.lua -set B100 -name 189080 -id 2 -lt 165_247 -wh 82_41
+
+
+DIV2K
+
+# th crop.lua -set val -name 0801 -id 1 -lt 777_194 -wh 53_106
+# th crop.lua -set val -name 0801 -id 2 -lt 769_176 -ps 178		##
+# th crop.lua -set val -name 0803 -id 1 -lt 1453_157 -wh 37_74
+# th crop.lua -set val -name 0803 -id 2 -lt 1214_443 -ps 158		##
+# th crop.lua -set val -name 0803 -id 3 -lt 1041_643 -ps 158		##
+# th crop.lua -set val -name 0803 -id 4 -lt 800_81 -ps 150		##
+# th crop.lua -set val -name 0804 -id 1 -lt 1321_342 -wh 35_70
+# th crop.lua -set val -name 0804 -id 2 -lt 836_440 -wh 43_86
+# th crop.lua -set val -name 0804 -id 3 -lt 1545_254 -wh 40_80
+# th crop.lua -set val -name 0804 -id 4 -lt 259_156 -wh 85_170
+# th crop.lua -set val -name 0804 -id 5 -lt 1703_389 -wh 70_140
+# th crop.lua -set val -name 0804 -id 6 -lt 705_387 -ps 62		##
+# th crop.lua -set val -name 0804 -id 7 -lt 719_452 -ps 42		##	
+# th crop.lua -set val -name 0804 -id 8 -lt 181_297 -ps 42		##
+# th crop.lua -set val -name 0804 -id 9 -lt 1626_748 -ps 99		##
+# th crop.lua -set val -name 0804 -id 10 -lt 1769_173 -ps 137		##
+# th crop.lua -set val -name 0805 -id 1 -lt 783_153 -wh 300_588
+# th crop.lua -set val -name 0805 -id 2 -lt 875_467 -wh 87_174
+# th crop.lua -set val -name 0805 -id 3 -lt 948_472 -wh 87_174
+# th crop.lua -set val -name 0805 -id 4 -lt 1037_171 -wh 94_188
+# th crop.lua -set val -name 0805 -id 5 -lt 493_258 -wh 94_188
+# th crop.lua -set val -name 0806 -id 1 -lt 1067_735 -wh 36_72
+# th crop.lua -set val -name 0806 -id 2 -lt 974_740 -wh 19_38
+# th crop.lua -set val -name 0806 -id 3 -lt 398_341 -wh 69_138
+# th crop.lua -set val -name 0806 -id 4 -lt 428_274 -ps 84		##
+# th crop.lua -set val -name 0806 -id 5 -lt 426_428 -ps 84		##
+# th crop.lua -set val -name 0806 -id 6 -lt 580_1118 -ps 187		##
+# th crop.lua -set val -name 0807 -id 1 -lt 1178_747 -wh 50_100
+# th crop.lua -set val -name 0807 -id 2 -lt 1578_731 -wh 26_52
+# th crop.lua -set val -name 0807 -id 3 -lt 1058_751 -wh 80_160
+# th crop.lua -set val -name 0807 -id 4 -lt 1005_455 -wh 55_110
+# th crop.lua -set val -name 0807 -id 5 -lt 1227_410 -wh 55_110
+# th crop.lua -set val -name 0807 -id 6 -lt 1026_774 -ps 110		##
+# th crop.lua -set val -name 0807 -id 7 -lt 1557_657 -ps 75		##
+# th crop.lua -set val -name 0808 -id 1 -lt 743_986 -wh 80_160
+# th crop.lua -set val -name 0808 -id 2 -lt 775_1005 -wh 37_74
+# th crop.lua -set val -name 0808 -id 3 -lt 1590_425 -wh 73_146
+# th crop.lua -set val -name 0808 -id 4 -lt 1672_452 -wh 36_72
+# th crop.lua -set val -name 0808 -id 5 -lt 1572_590 -wh 22_44
+# th crop.lua -set val -name 0808 -id 6 -lt 885_586 -ps 114		##
+# th crop.lua -set val -name 0808 -id 7 -lt 1583_456 -ps 96		##
+# th crop.lua -set val -name 0808 -id 8 -lt 1645_453 -ps 96		##
+# th crop.lua -set val -name 0809 -id 1 -lt 787_763 -wh 80_160
+# th crop.lua -set val -name 0809 -id 2 -lt 1215_261 -wh 80_160
+# th crop.lua -set val -name 0809 -id 3 -lt 1113_316 -wh 102_204
+# th crop.lua -set val -name 0809 -id 4 -lt 783_603 -wh 191_382
+# th crop.lua -set val -name 0810 -id 1 -lt 213_742 -wh 40_80
+# th crop.lua -set val -name 0810 -id 2 -lt 1293_1118 -wh 51_102
+# th crop.lua -set val -name 0810 -id 3 -lt 1536_464 -wh 180_360
+# th crop.lua -set val -name 0810 -id 4 -lt 1262_288 -wh 88_176
+
+# th crop.lua -set val -name 0811 -id 1 -lt 892_879 -ps 128
+# th crop.lua -set val -name 0811 -id 2 -lt 517_861 -ps 138
+# th crop.lua -set val -name 0812 -id 1 -lt 579_498 -ps 64
+# th crop.lua -set val -name 0812 -id 2 -lt 449_379 -ps 79
+# th crop.lua -set val -name 0812 -id 3 -lt 205_1066 -ps 63
+# th crop.lua -set val -name 0812 -id 4 -lt 1222_1225 -ps 59
+# th crop.lua -set val -name 0813 -id 1 -lt 677_531 -ps 165
+# th crop.lua -set val -name 0813 -id 2 -lt 682_148 -ps 115
+# th crop.lua -set val -name 0814 -id 1 -lt 1263_716 -ps 80
+# th crop.lua -set val -name 0814 -id 2 -lt 1074_1019 -ps 53
+# th crop.lua -set val -name 0814 -id 3 -lt 692_1006 -ps 66
+# th crop.lua -set val -name 0814 -id 4 -lt 913_721 -ps 57
+# th crop.lua -set val -name 0814 -id 5 -lt 1571_1040 -ps 60
+# th crop.lua -set val -name 0817 -id 1 -lt 907_588 -ps 152
+# th crop.lua -set val -name 0817 -id 2 -lt 1070_589 -ps 120
+# th crop.lua -set val -name 0817 -id 3 -lt 1545_1118 -ps 161
+# th crop.lua -set val -name 0818 -id 1 -lt 814_772 -ps 55
+# th crop.lua -set val -name 0818 -id 2 -lt 987_797 -ps 55
+# th crop.lua -set val -name 0820 -id 1 -lt 1688_852 -ps 118
+# th crop.lua -set val -name 0820 -id 2 -lt 1500_883 -ps 118
+# th crop.lua -set val -name 0820 -id 3 -lt 654_1092 -ps 118
+# th crop.lua -set val -name 0828 -id 1 -lt 1303_51 -ps 172
+# th crop.lua -set val -name 0828 -id 2 -lt 633_180 -ps 172
+# th crop.lua -set val -name 0828 -id 3 -lt 1027_558 -ps 172
+# th crop.lua -set val -name 0833 -id 1 -lt 1214_364 -ps 335
+# th crop.lua -set val -name 0833 -id 2 -lt 1732_320 -ps 168
+# th crop.lua -set val -name 0836 -id 1 -lt 1099_375 -ps 86
+# th crop.lua -set val -name 0833 -id 2 -lt 362_168 -ps 72
+# th crop.lua -set val -name 0833 -id 3 -lt 514_262 -ps 72
+# th crop.lua -set val -name 0851 -id 1 -lt 987_316 -ps 85
+# th crop.lua -set val -name 0892 -id 1 -lt 552_952 -ps 126
+# th crop.lua -set val -name 0892 -id 2 -lt 216_962 -ps 67
+
+th crop.lua -set Urban100 -name img004 -id 1 -lt 812_868 -ps 88
+th crop.lua -set Urban100 -name img004 -id 2 -lt 786_147 -ps 137
+th crop.lua -set Urban100 -name img005 -id 1 -lt 707_283 -ps 86
+th crop.lua -set Urban100 -name img010 -id 1 -lt 316_377 -ps 58
+th crop.lua -set Urban100 -name img019 -id 1 -lt 273_269 -ps 62
+th crop.lua -set Urban100 -name img021 -id 1 -lt 335_346 -ps 66
+th crop.lua -set Urban100 -name img021 -id 2 -lt 121_328 -ps 66
+th crop.lua -set Urban100 -name img039 -id 1 -lt 476_108 -ps 44
+th crop.lua -set Urban100 -name img039 -id 2 -lt 236_481 -ps 32
+
+th crop.lua -set Urban100 -name img039 -id 3 -lt 252_354 -wh 60_120
+th crop.lua -set Urban100 -name img039 -id 4 -lt 449_250 -wh 60_120
+th crop.lua -set Urban100 -name img039 -id 5 -lt 650_423 -wh 60_120
+th crop.lua -set Urban100 -name img041 -id 1 -lt 844_132 -wh 80_160
+th crop.lua -set Urban100 -name img046 -id 1 -lt 328_464 -wh 45_90
+th crop.lua -set Urban100 -name img048 -id 1 -lt 473_535 -wh 43_86
+th crop.lua -set Urban100 -name img057 -id 1 -lt 589_199 -wh 43_86
+th crop.lua -set Urban100 -name img057 -id 2 -lt 482_323 -wh 43_86
+th crop.lua -set Urban100 -name img059 -id 1 -lt 168_202 -wh 28_56
+th crop.lua -set Urban100 -name img061 -id 1 -lt 266_471 -wh 31_64
+th crop.lua -set Urban100 -name img061 -id 2 -lt 794_441 -wh 31_64
+th crop.lua -set Urban100 -name img067 -id 1 -lt 445_193 -wh 36_72
+
+
+
+
+
