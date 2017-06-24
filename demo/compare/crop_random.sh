176875ee914d4c4ae9477a02319c764c51b6dfbf
LimBee <forestrainee@gmail.com>
2017-06-25 08:11:44 +0900

    Randomly crops test images and sort

---
+++ demo/compare/crop_random.sh
@@ -0,0 +1,5 @@
+th crop_random.lua -set val -patchSize 50 -nSample 1e6
+th crop_random.lua -set val -patchSize 100 -nSample 1e6
+
+th crop_random.lua -set Urban100 -patchSize 50 -nSample 1e6
+th crop_random.lua -set Urban100 -patchSize 100 -nSample 1e6
